382e28311c7c3ad55da0c9a5cbb4d10317db5f3b
mhyvarinen <31437091+mhyvarinen@users.noreply.github.com>
2017-12-07 16:34:25 +0000

    Update syspref.js

--- templates/syspref.js
+++ templates/syspref.js
@@ -1,3 +1,9 @@
 user_pref("browser.startup.homepage", "http://haaga-helia.fi");
+user_pref("browser.privatebrowsing.autostart", true);
+user_pref("lightweightThemes.selectedThemeID", "firefox-compact-dark@mozilla.org");
+user_pref("browser.uiCustomization.state", "{"placements":{"widget-overflow-fixed-list":[],"PersonalToolbar":["personal-bookmarks"],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","customizableui-special-spring1","urlbar-container","search-container","customizableui-special-spring2","downloads-button","library-button","sidebar-button","ublock0_raymondhill_net-browser-action"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"toolbar-menubar":["menubar-items"],"addon-bar":["addonbar-closebutton","status-bar"]},"seen":["pocket-button","developer-button","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["PersonalToolbar","nav-bar","TabsToolbar","toolbar-menubar"],"currentVersion":12,"newElementCount":2}");
+user_pref("extensions.webextensions.uuids", "{"screenshots@mozilla.org":"27163524-96a3-4287-b87d-2ed94d42704c","uBlock0@raymondhill.net":"8ae93ac2-5f1a-4594-aa61-cefc2c03673e"}");
+
+
 
 
