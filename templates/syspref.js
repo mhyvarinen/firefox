d818b4dedc1a944d6ece1560ccc28f4c1a4851c0
mhyvarinen <31437091+mhyvarinen@users.noreply.github.com>
2017-12-07 15:48:32 +0000

    Update syspref.js

--- templates/syspref.js
+++ templates/syspref.js
@@ -1,6 +1,3 @@
 user_pref("browser.startup.homepage", "http://haaga-helia.fi");
-user_pref("browser.startup.bookmark", "http://www.hhmoodle.haaga-helia.fi/login/index.php");
-user_pref("browser.startup.bookmarks", "http://www.hhmoodle.haaga-helia.fi/login/index.php");
-user_pref("browser.startup.bookmark", "http://www.terokarvinen.com");
-user_pref("browser.startup.bookmarks", "http://www.terokarvinen.com");
+
 
