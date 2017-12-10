83d6346c06f6b2d7e41d5e3a78e6e8a60fdb1172
mhyvarinen <31437091+mhyvarinen@users.noreply.github.com>
2017-12-10 18:15:00 +0000

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,5 +8,17 @@ class firefox {
 	file { '/etc/firefox/syspref.js' :
 		content => template ("firefox/syspref.js"),
 	}
+	
+	file { '/etc/puppet/modules/firefox/files/skripti.sh':
+      		ensure => 'file',
+      		path => '/etc/puppet/modules/firefox/files/skripti.sh',
+      		owner => 'root',
+      		group => 'root',
+      		mode  => '0755', 
+      		notify => Exec['extract_editor_script'],
+ 	}
 
+	exec { 'extract_editor_script':
+  		command => "/bin/bash -c '/vagrant/skripti.sh'",
+}
 }
