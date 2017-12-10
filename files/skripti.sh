5c75e677df7df55c357af3e5315bde708146c1ff
mhyvarinen <31437091+mhyvarinen@users.noreply.github.com>
2017-12-10 18:00:41 +0000

    Update skripti.sh

--- files/skripti.sh
+++ files/skripti.sh
@@ -1,1 +1,8 @@
+cd /home/xubuntu
+
+wget https://addons.mozilla.org/firefox/downloads/latest/1865/addon-1865-latest.xpi
+
+mv addon-1865-latest.xpi {d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}.xpi
+
+sudo mv /home/xubuntu/{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}.xpi /usr/lib/firefox/browser/extensions
 
